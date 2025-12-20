046ee2491ecc333faa1f20b8235c08aaee960bfe
Sebastian <s092715@student.tu.kielce.pl>
2025-12-20 19:03:07 +0100

    dockerfile

--- backend/DjangoProject/settings.py
+++ backend/DjangoProject/settings.py
@@ -55,11 +55,11 @@ ROOT_URLCONF = 'DjangoProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'root/backend/templates']
-        ,
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -68,19 +68,21 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'repo-root.wsgi.application'
-
+WSGI_APPLICATION = 'DjangoProject.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'appdb',
+        'USER': 'appuser',
+        'PASSWORD': 'apppass',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
